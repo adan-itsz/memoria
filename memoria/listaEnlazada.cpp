340ec35476a996f2140a6bf07e81d9df26209ba6
Adan De Santiago <adan1995a@gmail.com>
2016-04-19 10:31:17 -0500

    agrege clase lista enlazada

---
+++ memoria/listaEnlazada.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "listaEnlazada.h"
+
+
+listaEnlazada::listaEnlazada()
+{
+
+}
+
+
+listaEnlazada::~listaEnlazada()
+{
+}
---
+++ memoria/listaEnlazada.h
@@ -0,0 +1,11 @@
+#pragma once
+class listaEnlazada
+{
+public:
+	listaEnlazada();
+
+
+
+	~listaEnlazada();
+};
+
